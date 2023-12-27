84ea5e25d497b6660fd43a47dd5a9a3f3947739a
Chew <u2005289@siswa.um.edu.my>
2023-12-28 01:50:49 +0800

    updated dfs

--- src/main/java/examples/StarterGhostComm/DFSGhost.java
+++ src/main/java/examples/StarterGhostComm/DFSGhost.java
@@ -9,6 +9,9 @@ import pacman.game.Game;
 
 public class DFSGhost extends IndividualGhostController {
     private final Constants.GHOST ghost;
+    private static final int PACMAN_DISTANCE = 15;
+    private static final int PILL_PROXIMITY = 15;
+
     public DFSGhost(Constants.GHOST ghost) {
         super(ghost);
         this.ghost = ghost;
@@ -49,6 +52,44 @@ public class DFSGhost extends IndividualGhostController {
             }
         }
 
+        if (closeToPower(game)) {
+            // Perform an action when close to a power pill
+            // For example, move away from Ms. Pac-Man
+            return game.getNextMoveAwayFromTarget(currentIndex, pacmanIndex, Constants.DM.PATH);
+        }
+        
+        if (closeToMsPacman(game, currentIndex)) {
+            // Perform an action when close to Ms. Pac-Man
+            // For example, move towards Ms. Pac-Man
+            return game.getNextMoveTowardsTarget(currentIndex, pacmanIndex, Constants.DM.PATH);
+        }
+
         return MOVE.NEUTRAL; // No path found or other conditions, return a default move
+    }
+
+    // Helper methods for distance calculation, proximity checks, etc.
+    private boolean closeToPower(Game game) {
+        int[] powerPills = game.getPowerPillIndices();
+        int pacmanNodeIndex = game.getPacmanCurrentNodeIndex();
+
+        for (int powerPillIndex : powerPills) {
+            Boolean powerPillStillAvailable = game.isPowerPillStillAvailable(powerPillIndex);
+
+            if (powerPillStillAvailable != null && powerPillStillAvailable) {
+                int distance = game.getShortestPathDistance(powerPillIndex, pacmanNodeIndex);
+                if (distance != -1 && distance < PILL_PROXIMITY) {
+                    return true; // Found a power pill close to Ms. Pac-Man
+                }
+            }
+        }
+
+        return false; // No power pill close to Ms. Pac-Man
+    }
+
+    private boolean closeToMsPacman(Game game, int location) {
+        int pacmanNodeIndex = game.getPacmanCurrentNodeIndex();
+        int distance = game.getShortestPathDistance(pacmanNodeIndex, location);
+
+        return distance != -1 && distance < PACMAN_DISTANCE;
     }
 }
